0745f2b4da644f7f55dc56c43d144b759205327e
Peter Golm <golm.peter@gmail.com>
2014-01-21 12:00:44 +0100

    improve pubkey funtions thx @LAndreas

--- zsh/system/path.zsh
+++ zsh/system/path.zsh
@@ -3,3 +3,6 @@ export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
 export PATH="$(brew --prefix josegonzalez/php/php55)/bin:$PATH"
+
+# your ssh folder for pubkey command
+export SSH_KEY_PATH="$HOME/.ssh"

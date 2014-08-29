59d120209d9e69238240488d02d903f8d50d8942
Peter Golm <golm.peter@gmail.com>
2014-08-29 23:51:18 +0200

    ...

--- zsh/00_system/path.zsh
+++ zsh/00_system/path.zsh
@@ -1,6 +1,9 @@
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$HOME/.bin:$PATH"
+export PATH="./bin:$HOME/.rbenv/shims:$HOME/.sfs:$HOME/.bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
 
 export PATH="$(brew --prefix josegonzalez/php/php55)/bin:$PATH"
 

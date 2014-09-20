5f4b65d76077a3ec8a509bb7703f19fd6983013b
Peter Golm <golm.peter@gmail.com>
2014-09-20 08:59:57 +0200

    code cleanup

--- zsh/00_system/aliases.zsh
+++ zsh/00_system/aliases.zsh
@@ -2,17 +2,17 @@ alias reload!='. ~/.zshrc'
 alias ll='ls -al'
 
 # Bundler
-alias be="bundle exec"
-alias bi="bundle install"
-alias br="bundle rspec"
+alias be='bundle exec'
+alias bi='bundle install'
+alias br='bundle rspec'
 
 # grc overides for ls
 #   Made possible through contributions from generous benefactors like
 #   `brew install coreutils`
 if $(gls &>/dev/null)
 then
-  alias ls="gls -F --color"
-  alias l="gls -lAh --color"
-  alias ll="gls -l --color"
+  alias ls='gls -F --color'
+  alias l='gls -lAh --color'
+  alias ll='gls -l --color'
   alias la='gls -A --color'
 fi

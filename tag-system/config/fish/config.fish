5d73c934e9e815b4c0f2385d3fff7903a2346946
Peter Golm <golm.peter@gmail.com>
2015-09-16 22:34:28 +0200

    Add autojump

--- tag-system/config/fish/config.fish
+++ tag-system/config/fish/config.fish
@@ -1,6 +1,8 @@
 set -gx DOTFILES_DIR ~/.dotfiles
 set -gx PATH ~/.bin $PATH
 
+[ -f /usr/local/share/autojump/autojump.fish ]; and . /usr/local/share/autojump/autojump.fish
+
 # rbenv setup
 set -gx RBENV_ROOT /usr/local/var/rbenv
 . (rbenv init - fish)

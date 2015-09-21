55f75a66a196dfeded1a30c76132d787c94ae651
Peter Golm <golm.peter@gmail.com>
2015-09-21 23:56:40 +0200

    Some refactoring

--- tag-fish/config/fish/config.fish
+++ tag-fish/config/fish/config.fish
@@ -1,9 +1,10 @@
-set -gx DOTFILES_DIR ~/.dotfiles
 set -gx PATH ~/.bin $PATH
 
 [ -f /usr/local/share/autojump/autojump.fish ]; and . /usr/local/share/autojump/autojump.fish
 
 # rbenv setup
-set PATH $HOME/.rbenv/bin $PATH
-set PATH $HOME/.rbenv/shims $PATH
-. (rbenv init -|psub)
+if status -–is-interactive
+  set PATH $HOME/.rbenv/bin $PATH
+  set PATH $HOME/.rbenv/shims $PATH
+  . (rbenv init -|psub)
+end

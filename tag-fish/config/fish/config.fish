356153a9017bf5410ce0f6f24124b16dadd4d0e4
Peter Golm <golm.peter@gmail.com>
2015-09-23 00:37:39 +0200

    Remove rbenv check

--- tag-fish/config/fish/config.fish
+++ tag-fish/config/fish/config.fish
@@ -4,8 +4,6 @@ set -gx PATH ~/.bin $PATH
 [ -f /usr/local/share/autojump/autojump.fish ]; and . /usr/local/share/autojump/autojump.fish
 
 # rbenv setup
-if status -–is-interactive
-  set PATH $HOME/.rbenv/bin $PATH
-  set PATH $HOME/.rbenv/shims $PATH
-  . (rbenv init -|psub)
-end
+set PATH $HOME/.rbenv/bin $PATH
+set PATH $HOME/.rbenv/shims $PATH
+. (rbenv init -|psub) 2 > /dev/null
